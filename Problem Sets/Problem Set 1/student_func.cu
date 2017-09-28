cccea3ed624506cb6bc35baae018cf1136cda149
¨yuwang¨ <yuw061987@gmail.com>
2017-09-28 21:29:18 +0200

    Problem set1: opencv path changes and opencv3 related changes in makefile

--- Problem Sets/Problem Set 1/student_func.cu
+++ Problem Sets/Problem Set 1/student_func.cu
@@ -50,6 +50,15 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+  const int2 p = make_int2(blockIdx.x * blockDim.x + threadIdx.x,
+                           blockIdx.y * blockDim.y + threadIdx.y);
+  const int idx = p.y * numCols + p.x;
+    
+  if(p.x >= numCols || p.y >= numRows)
+  return;
+
+  greyImage[idx] = .299f *rgbaImage[idx].x  + .587f * rgbaImage[idx].y + .114f * rgbaImage[idx].z;
+  
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -57,8 +66,8 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+  const dim3 blockSize(32, 32, 1);  //TODO
+  const dim3 gridSize( numCols/32+1, numRows/32+1, 1);  //TODO
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
